657aa31bf286270027c63cbd57f51c1872be7e99
Chonel LaPorte <chonellaporte@OSXLAP01612.local>
2014-02-11 22:25:49 -0500

    hud with blank placeholder items

--- src/components/player_components/player.js
+++ src/components/player_components/player.js
@@ -12,7 +12,7 @@ Crafty.c('Player', {
 	_thirst: 30,
 	_playerHUD: null,
 	_followers: [1,1,1,1,1,1,1,1],
-	_tradeItems: ['fig', 'incense', 'mur'],
+	_tradeItems: ['Gold', 'Gold', 'Gold'],
 	_money: 000000000000,
 	init: function() {
 		this.requires('Actor, Fourway, Collision, Delay, FPS, Persist, Solid')
@@ -180,10 +180,18 @@ Crafty.c('Player', {
 			hud._hpBar.w = this._hitPoints * 2;
 			hud._thirstBar.w = this._thirst * 2;
 			hud._money.text(this._money);
-		}
-		if (this._tradeItems.join() !== hud._tradeItems.join()) {
-			hud._tradeItems = this._tradeItems;
-			hud.createTradeItems();
+			if (this._tradeItems.join() !== hud._tradeItems.join()) {
+				hud._tradeItems = this._tradeItems;
+				for (var x = 0; x <= 2; x++) {
+					if(hud._tradeItems[x]) {
+						Crafty(hud._tradeItemsEntities[x][0]).addComponent(hud._tradeItems[x]);
+					} else {
+						console.log(hud._tradeItems[x])
+						Crafty(hud._tradeItemsEntities[x][0]).removeComponent('Gold', false);
+						console.log(Crafty(hud._tradeItemsEntities[x][0]).draw())
+					}
+				}
+			}
 		}
 	}
 })
@@ -251,90 +259,5 @@ Crafty.c('WhiteCharacter', {
 			var actor = this.hit('Actor')[0].obj;
 			actor._hitPoints -= 1;
 		}
-	}
-})
-
-Crafty.c('HUDElement', {
-	init: function () {
-		this.requires('2D, DOM, Persist')
-	}
-})
-
-Crafty.c('HUD', {
-	_hpBar: null,
-	_thirstBar: null,
-	_camels: null,
-	_money: null,
-	_selectedWeapon: null,
-	_tradeItemsContainer: null,
-	_tradeItems: ['cat','rat','ball'],
-	_tradeItemsEntities: [],
-	init: function () {
-		this.requires('HUDElement');
-		// this.attr({x:10, y:10, h:30, w:0});
-		this.createHpBar();
-		this.createThirstBar();
-		this.money();
-		this.createSelectedWeapon();
-		this.createTradeItemsContainer();
-		this.createTradeItems();
-	},
-
-	createHpBar: function () {
-		// var heart = Crafty.e('HUDElement, Color').color('red').attr({x:10, y:10, w:12, h:15});
-		this._hpBar = Crafty.e('HUDElement, Color, Text');
-		this._hpBar.attr({x:45, y:10, h:10, w:0});
-		this._hpBar.color('red');
-	},
-
-	createThirstBar: function () {
-		// var waterdrop = Crafty.e('HUDElement, Color').color('blue').attr({x:53, y:10, w:12, h:15});
-		this._thirstBar = Crafty.e('HUDElement, Color, Text');
-		this._thirstBar.attr({x:45, y:25, h:10, w:0});
-		this._thirstBar.color('blue');
-	},
-
-
-	createSelectedWeapon: function () {
-		this._selectedWeapon = Crafty.e('HUDElement, Color');
-		this._selectedWeapon.attr({x:10, y:10, h:25, w:25});
-		this._selectedWeapon.css({'border':'solid black 2px', 'border-radius': '5px'});
-	},
-
-	createTradeItemsContainer: function () {
-		this._tradeItemsContainer = Crafty.e('HUDElement, Color');
-		this._tradeItemsContainer.attr({x:10, y:42, h:24, w:92});
-		this._tradeItemsContainer.css({'border':'solid black 2px', 'border-radius': '5px'});
-	},
-
-	createTradeItems: function () {
-		var itemArray = [];
-		for (var x = 0; x < this._tradeItems.length; x++) {
-			if (this._tradeItemsEntities[x]) {
-				console.log(this._tradeItemsEntities[x][0]);
-				console.log(Crafty(this._tradeItemsEntities[x][0]));
-				Crafty(this._tradeItemsEntities[x][0]).destroy();
-			}
-			var item = Crafty.e('HUDElement, Color, Mouse');
-			// item.color('gray')
-			item.css({'border':'solid black 2px', 'border-radius': '5px'});
-			item.attr({x:this._tradeItemsContainer.x + 5 + (20 * x), y:this._tradeItemsContainer.y + 5, h:15, w:15});
-			item._type = this._tradeItems[x];
-			item.bind('Click', function() {
-				console.log(this);
-			})
-			this._tradeItemsEntities.push(item);
-		}
-	},
-
-	money: function () {
-		var gold = Crafty.e('HUDElement, Color, Text');
-		gold.attr({x:10, y:72});
-		gold.text('Gold');
-		gold.textFont({size: '12px', weight: 'bold'});
-		this._money = Crafty.e('HUDElement, Color, Text');
-		this._money.attr({x:50, y:72});
-		this._money.textColor('#007F00');
-		this._money.textFont({size: '12px', weight: 'bold'});
 	}
 })
